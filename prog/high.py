be24c3aa94cdeaa923f06ebd4087988237cd5a57
FiaLDI <leon.youtube2017@yandex.ru>
2023-10-14 11:19:57 +0300

    add order and redact code

--- prog/high.py
+++ prog/high.py
@@ -1,14 +1,26 @@
 
 ## Вариант 17(7)
 
-from cmath import pi
+import math
 
+def solve_clock_problem(y):
+    # Вычисление угла часовой стрелки
+    angle_hour = y * 360 / (2 * math.pi)
+    
+    # Вычисление угла минутной стрелки
+    angle_minute = angle_hour * 2
+    
+    # Определение количества полных часов
+    full_hours = round(angle_hour / 30)
+    
+    # Определение количества полных минут
+    full_minutes = round(angle_minute / 6)
+    
+    return angle_minute, full_hours, full_minutes
 
 if __name__ == "__main__":
-    y = int(input())
-    tanmin = (y * 60) / 2 * pi
-    fulltime = tanmin // (pi / 6)
-    fullmin = (tanmin % (pi/6)) * 2
-    print(f"Угол минутной стрелки - {tanmin}")
-    print(f"Полные часы - {fulltime}")
-    print(f"Полные минуты - {fullmin}")
+    y = float(input("y = "))
+    angle_minute, full_hours, full_minutes = solve_clock_problem(y)
+    print(f"Угол для минутной стрелки: {angle_minute}")
+    print(f"Количество полных часов: {full_hours}")
+    print(f"Количество полных минут: {full_minutes}")
